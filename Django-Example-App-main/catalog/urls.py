1d95e1b4bd1676448e20daea44e583f4853f4db0
skeka <shreker240@gmail.com>
2022-01-26 18:38:48 +0200

    new

---
+++ Django-Example-App-main/catalog/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("books", views.BookListView.as_view(), name="books"),
+    path("authors", views.AuthorListView.as_view(), name="authors"),
+    path("authors/<int:pk>", views.AuthorDetailView.as_view(), name="authors"),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book"),
+
+]
